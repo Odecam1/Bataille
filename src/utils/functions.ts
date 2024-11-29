0e6542e3ee62846b005887de2e97ab28249d6b1a
Odecam1 <timotheedanymacedo@gmail.com>
2024-11-29 12:11:40 +0100

    add the shuffleArray to shuffle the cards

---
+++ src/utils/functions.ts
@@ -0,0 +1,9 @@
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+export const shuffleArray = <T>(array: T[]): T[] => {
+  const shuffledArray = [...array]
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
+  }
+  return shuffledArray
+}
